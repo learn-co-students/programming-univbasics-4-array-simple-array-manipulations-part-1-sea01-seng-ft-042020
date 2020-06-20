72fb5d2653626560a9225b96ad83fbc0ed4fb9a3
github username <github email address>
2020-06-19 19:37:39 -0700

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_push(a, str)
+  a.push(str)
+end
+
+def using_unshift(a,str)
+  a.unshift(str)
+end
+
+def using_pop(a)
+  a.pop
+end
+
+def pop_with_args(a)
+  a.pop(2)
+end
+
+def using_shift(a)
+  a.shift
+end
+
+def shift_with_args(a)
+  a.shift(2)
+end
+
